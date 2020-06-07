9c91ec90b592425393a1d8bdc63b2ea75e6c41b1
Tanya Hodges <tanyac52003@gmail.com>
2020-06-07 19:06:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,6 @@ class Song
   
   @@count = 0 
   
-  
   def self.count
     @@count
   end 
@@ -31,10 +30,14 @@ def self.artists
   @@artists.uniq 
 end 
 
-def initialize
-
-  
-  
+def initialize(name, artis_name, genre)
+  @name = name 
+  @artist = artist 
+  @@artists << artist
+  @genre = genre 
+  @@genres << genre 
+  @@count += 1 
+end 
 end 
 
 
